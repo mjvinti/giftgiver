f62880803869fad119639f6f4cd03a01c2db4a4d
Mario <mjvinti@gmail.com>
2019-06-21 10:38:32 -0400

    rendering the gift list in the App

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,6 +4,16 @@ import App from './App';
 
 const app = shallow(<App />);
 
-it('renders correctly', () => {
+it('should render correctly', () => {
   expect(app).toMatchSnapshot();
+});
+
+it('should initialize the `state` with an empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([]);
+});
+
+it('should add a new gift to `state` when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
