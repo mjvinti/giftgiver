f8aa37ec1ab06ced9f51b7e2c64d69597365f9b2
Mario <mjvinti@gmail.com>
2019-06-21 11:14:04 -0400

    restructured test suite to DBB approach and added afterEach / beforeEach cleanup

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,24 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
+describe('App', () => {
+  const app = shallow(<App />);
 
-it('should render correctly', () => {
-  expect(app).toMatchSnapshot();
-});
+  it('should render correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it('should initialize the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([]);
-});
+  it('should initialize the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it('should add a new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
+  describe('when clicking the add `add-gift` button', () => {
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
 
-  expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+    
+    it('should add a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }]);
+    });
 
-it('should add a new gift to the rendered list when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
-
-  expect(app.find('.gift-list').children().length).toEqual(2);
+    it('should add a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+  });
 });
